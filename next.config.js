698d2a633b87c7bc2bbce34687fdb9e73ca8a0f7
John Doe <johndoe@example.com>
2023-05-05 14:37:04 +0530

    main pushfvf

--- next.config.js
+++ next.config.js
@@ -1,8 +1,9 @@
-/** @type {import('next').NextConfig} */
-const nextConfig = {
-  experimental: {
-    appDir: true,
-  },
-}
-
-module.exports = nextConfig
+module.exports = {
+  exportPathMap: function() {
+    return {
+      '/': { page: '/' },
+      '/about': { page: '/about' },
+      // add more pages as needed
+    };
+  }
+};
